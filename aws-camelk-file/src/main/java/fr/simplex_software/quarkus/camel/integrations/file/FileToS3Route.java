c7ae01b43dcf228c12f481c33394971fcde50413
Nicolas Duminil <nicolas.duminil@simplex-software.fr>
2023-04-25 13:50:23 +0200

    Have added properties

--- aws-camelk-file/src/main/java/fr/simplex_software/quarkus/camel/integrations/file/FileToS3Route.java
+++ aws-camelk-file/src/main/java/fr/simplex_software/quarkus/camel/integrations/file/FileToS3Route.java
@@ -1,12 +1,11 @@
 package fr.simplex_software.quarkus.camel.integrations.file;
 
-import org.apache.camel.*;
 import org.apache.camel.builder.*;
 import org.apache.camel.component.aws2.s3.*;
 import org.apache.camel.component.file.*;
+import org.eclipse.microprofile.config.inject.*;
 
 import javax.enterprise.context.*;
-import javax.inject.*;
 import java.io.*;
 import java.util.*;
 
@@ -19,18 +18,18 @@ public class FileToS3Route extends RouteBuilder
     .limit(5)
     .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
     .toString();
-  private static final String AWS_S3_URI_TEMPLATE = "aws2-s3://myBucket%s?autoCreateBucket=true&useDefaultCredentialsProvider=true&prefix=hello.txt";
-  private static final String AWS_S3_URI = String.format(AWS_S3_URI_TEMPLATE, RANDOM);
-  @Inject
-  private CamelContext context;
+  @ConfigProperty(name="inBox")
+  private String inBox;
+  @ConfigProperty(name="s3Name")
+  private String s3Name;
 
   public void configure() throws Exception
   {
     onException(IOException.class)
       .handled(true)
       .log("IOException occurred due: ${exception.message}");
-    from("file://tmp/input?delete=true&idempotent=true&bridgeErrorHandler=true")
+    fromF("file://%s?delete=true&idempotent=true&bridgeErrorHandler=true", inBox)
       .setHeader(AWS2S3Constants.KEY, header(FileConstants.FILE_NAME))
-      .to(aws2S3("mybucket" + RANDOM).autoCreateBucket(true).useDefaultCredentialsProvider(true));
+      .to(aws2S3(s3Name + RANDOM).autoCreateBucket(true).useDefaultCredentialsProvider(true));
   }
 }
