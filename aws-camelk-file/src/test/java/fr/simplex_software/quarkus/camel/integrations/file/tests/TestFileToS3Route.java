5b6cf06fdde7a45a70464f41600799d7894b6b5d
Nicolas Duminil <nicolas.duminil@simplex-software.fr>
2023-04-27 00:18:40 +0200

    Have updated TestFileToS3Route such that to add support for removing test buckets

--- aws-camelk-file/src/test/java/fr/simplex_software/quarkus/camel/integrations/file/tests/TestFileToS3Route.java
+++ aws-camelk-file/src/test/java/fr/simplex_software/quarkus/camel/integrations/file/tests/TestFileToS3Route.java
@@ -11,20 +11,45 @@ import java.util.*;
 import static org.assertj.core.api.Assertions.*;
 
 @QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TestFileToS3Route
 {
   @EndpointInject("file://tmp/input?delete=true&idempotent=true")
   ProducerTemplate producerTemplate;
+  private static AmazonS3 amazonS3Client;
+  private static List<Bucket> buckets;
+
+  @BeforeAll
+  public static void beforeAll()
+  {
+    amazonS3Client = AmazonS3ClientBuilder.standard().build();
+    buckets = amazonS3Client.listBuckets();
+  }
+
+  @AfterAll
+  public static void afterAll()
+  {
+    amazonS3Client = null;
+    buckets = null;
+  }
 
   @Test
+  @Order(10)
   public void testStoreFileToS3() throws InterruptedException
   {
     producerTemplate.sendBodyAndHeader("Test", Exchange.FILE_NAME, "test.txt");
     Thread.sleep(2000);
-    AmazonS3 s3client = AmazonS3ClientBuilder.standard().build();
-    List<Bucket> buckets = s3client.listBuckets();
     assertThat(buckets).isNotNull();
     assertThat(buckets.size()).isGreaterThan(0);
     assertThat(buckets.stream().anyMatch(b -> b.getName().startsWith("mys3"))).isTrue();
+  }
+
+  @Test
+  public void testRemoveS3Bucket()
+  {
+    String bucketName = buckets.stream().filter(b -> b.getName().startsWith("mys3")).findFirst().orElseThrow().getName();
+    assertThat(bucketName).isNotNull();
+    amazonS3Client.listObjects(bucketName).getObjectSummaries().forEach(os -> amazonS3Client.deleteObject(bucketName, os.getKey()));
+    amazonS3Client.deleteBucket(bucketName);
   }
 }
