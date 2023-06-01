8578724813b4c2a6f5989bbd93fe2abf17c4f2f3
Nicolas Duminil <nicolas.duminil@simplex-software.fr>
2023-06-01 18:16:16 +0200

    Have removed unused class

--- kill-ms.sh
+++ kill-ms.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 kill $(cat pid-aws-camelk-file.pid)
-kill $(cat pid-aws-camelk-s3.pid)
-#kill $(cat pid-aws-camelk-jaxrs.pid)
+kill $(cat pid-aws-camelk-jaxrs.pid)
 kill $(cat pid-aws-camelk-sqs.pid)
+kill $(cat pid-aws-camelk-s3.pid)
+
