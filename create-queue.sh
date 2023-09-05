a2883a70dd3968239e7e5b89a34622b5efcc4fd2
Nicolas DUMINIL <nicolas.duminil@simplex-software.fr>
2023-09-05 14:37:13 +0200

    Have fixed minor issue in start-ms.sh

--- create-queue.sh
+++ create-queue.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 QUEUE_LIST=$(aws sqs list-queues --query "QueueUrls")
-if [ '$QUEUE_LIST' = 'null' ]
+if [[ $QUEUE_LIST = null ]]
 then
   aws sqs create-queue --queue-name myQueue
 else
--- start-ms.sh
+++ start-ms.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 ./delete-all-buckets.sh
 ./create-queue.sh
+sleep 10
 mvn -DskipTests -Dquarkus.kubernetes.deploy clean package
 sleep 3
 ./copy-xml-file.sh
